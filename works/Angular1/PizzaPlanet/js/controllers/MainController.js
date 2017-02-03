48292d612e60bf82c5a45d190bf6a76fedb9e02e
ashish9342 <ashish9342@gmail.com>
2017-02-03 11:47:59 +0530

    created basics app pizza planet

--- works/Angular1/PizzaPlanet/js/controllers/MainController.js
+++ works/Angular1/PizzaPlanet/js/controllers/MainController.js
@@ -1,45 +1,18 @@
-app.controller('MainController',['$scope', function($scope){
-  $scope.title = 'Books on Books';
-  $scope.promo = 'Too Many Books';
-  $scope.products = [
-  {
-    name: 'The Book of Trees',
-    price: 19,
-    pubdate: new Date('2014', '03', '08'),
-    cover: 'img/the-book-of-trees.jpg',
-    likes:0,
-    dislikes:0
-  },
-  {
-    name: 'Program or be Programmed',
-    price: 8,
-    pubdate: new Date('2013', '08', '01'),
-    cover: 'img/program-or-be-programmed.jpg',
-    likes:0,
-    dislikes:0
-  },
-  {
-    name: 'The Book of Trees',
-    price: 19,
-    pubdate: new Date('2014', '03', '08'),
-    cover: 'img/the-book-of-trees.jpg',
-    likes:0,
-    dislikes:0
-  },
-  {
-    name: 'Program or be Programmed',
-    price: 8,
-    pubdate: new Date('2013', '08', '01'),
-    cover: 'img/program-or-be-programmed.jpg',
-    likes:0,
-    dislikes:0
-  }
-];
-  	$scope.plusOne = function(index){
-      $scope.products[index].likes += 1;
-    };
-    $scope.minusOne = function(index){
-      $scope.products[index].dislikes -= 1;
-    }
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.today = new Date();
+
+    $scope.appetizers = [{
+        name: 'Caprese',
+        description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+        price: 4.95
+    }, {
+        name: 'Mozzarella Sticks',
+        description: 'Served with marinara sauce.',
+        price: 3.95
+    }, {
+        name: 'Bruschetta',
+        description: 'Grilled bread garlic, tomatoes, olive oil.',
+        price: 4.95
+    }];
 
 }]);
