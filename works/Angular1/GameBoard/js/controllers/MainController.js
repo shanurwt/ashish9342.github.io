8d413f6f1e088394015320299529e19586b0dcbe
ashish9342 <ashish9342@gmail.com>
2017-02-05 23:21:57 +0530

    Updated images and the path im mainController

--- works/Angular1/GameBoard/js/controllers/MainController.js
+++ works/Angular1/GameBoard/js/controllers/MainController.js
@@ -44,10 +44,10 @@ app.controller('MainController', ['$scope', function($scope) {
             price: 1.99
         },
         {
-            icon: 'img/shutterbugg.jpg',
-            title: 'Shutterbugg',
-            developer: 'Chico Dusty',
-            price: 2.99
+            icon: 'img/forecast.jpg',
+            title: 'Forecast',
+            developer: 'Ruby',
+            price: 3.99
         }
     ]
 }]);
